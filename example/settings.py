60b9f5ae2ba7bd019ab98354addeb02eed3d9917
exponentian <exponentian.oh@gmail.com>
2020-07-30 17:09:02 -0700

    edited requirements.txt

--- example/settings.py
+++ example/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes'
+)
